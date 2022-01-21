df3c9427a604b102f46f19e7c6323b42c8f428c7
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2022-01-21 15:41:38 +0530

    Create Inspiration.js

---
+++ src/components/inspiration/Inspiration.js
@@ -0,0 +1,13 @@
+import Workpage from "../landing/work page/work_page";
+
+const Inspiration = () => {
+  return (
+    <>
+      <div style={{marginTop: '10vh'}}>
+        <Workpage />
+      </div>
+    </>
+  );
+};
+
+export default Inspiration;
