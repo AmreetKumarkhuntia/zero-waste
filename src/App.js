f12e61406db517c726650b614d138b40f0664076
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2022-01-21 13:02:54 +0530

    package update
    packages update with resources folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Booknow from "./components/booknow/Booknow";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Inspiration from "./components/inspiration/Inspiration";
+import Landing from "./components/landing/landing";
+import Ngo from "./components/ngopage/ngo";
+import Pickup from "./components/Pickup/Pickup";
+
+import "./resources/App.css";
 
 function App() {
   return (
-    <div className="App">
-      sss
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/pickup" component={Pickup} />
+        <Route exact path="/book" component={Booknow} />
+        <Route exact path="/inspiration" component={Inspiration} />
+        <Route exact path="/ngo" component={Ngo} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
