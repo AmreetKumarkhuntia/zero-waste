e586e9b63958be8ac44b4d8b00ee914cacdc7ba8
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2022-01-21 15:42:19 +0530

    Work Page

---
+++ src/components/landing/work page/tweets.jsx
@@ -0,0 +1,26 @@
+const tweets = [
+    {
+        id:1,
+        icon:"https://res.cloudinary.com/dke9xzbl6/image/upload/v1641629534/ze-te/Amreet_kumar_khuntia_2_qp4mot.png",
+        name:"Amreet Kumar Khuntia",
+        work:"Front-end Developer, Designer",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio. Nisl suscipit adipiscing bibendum est ultricies integer quis. Odio aenean sed adipiscing diam donec. Volutpat commodo sed egestas egestas fringilla."
+    },
+    {
+        id:2,
+        icon:"https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638359763/shantanu-portfolio/Kumar_Shantanu_y3ktwt.jpg",
+        name:"Kumar Shantanu",
+        work:"Front-end Developer, Designer",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio. Nisl suscipit adipiscing bibendum est ultricies integer quis. Odio aenean sed adipiscing diam donec. Volutpat commodo sed egestas egestas fringilla."
+    },
+    {
+        id:3,
+        icon:"https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638359763/shantanu-portfolio/Kumar_Shantanu_y3ktwt.jpg",
+        name:"Shreeansh Praharaj",
+        work:"Content Writer, Designer",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio. Nisl suscipit adipiscing bibendum est ultricies integer quis. Odio aenean sed adipiscing diam donec. Volutpat commodo sed egestas egestas fringilla."
+    },
+    
+]
+
+export {tweets};
---
+++ src/components/landing/work page/work_page.js
@@ -0,0 +1,71 @@
+import './css/Workpage.css';
+import quotes from './css/Quote Mark.svg';
+
+import { tweets } from './tweets';
+import { useState } from 'react';
+import left_arrow from './css/Left Arrow.svg';
+import right_arrow from './css/Right Arrow.svg';
+
+const Work_page = () => {
+
+    const max_index=1;
+
+    const [index, setIndex] = useState(0);
+
+    function next(){
+        let temp=index;
+        if(temp<max_index)
+            temp++;
+        setIndex(temp);
+    }
+    function prev(){
+        let temp=index;
+        if(temp>0)
+            temp--;
+        setIndex(temp);
+    }
+
+    return (
+        <>
+            <div className="Work_page">
+                <div className="left">
+                    <div>
+                        <div className='heading'>Why we work?</div>
+                        <div className='subheading'>Our inspiration</div>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className='tweets'>
+                        <div className='quotes'><img src={quotes} alt='error loading'></img></div>
+                        <div className='tweet'>
+                            <div className='head'>
+                                <img className='icon' src={tweets[index].icon} alt="error loading"></img>
+                                <div>
+                                    <div className='name'>
+                                        {tweets[index].name}
+                                    </div>
+                                    <div className='work'>
+                                        {tweets[index].work}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='sub_head'>
+                                {tweets[index].description}
+                            </div>
+                        </div>
+                        <div className='navigation_button'>
+                            <button className='goright' onClick={()=>{
+                                next();
+                            }}><img src={right_arrow} alt="error"></img></button>
+                            <button className='goleft'onClick={()=>{
+                                prev();
+                            }} ><img src={left_arrow} alt="error"></img></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Work_page;
